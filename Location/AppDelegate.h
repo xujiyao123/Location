efa6d0e6721f41e8ff7864cbf7b08542433282fc
徐继垚 <422242216@qq.com>
2015-07-06 10:33:37 +0800

    Initial Commit

---
+++ Location/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Location
+//
+//  Created by 徐继垚 on 15/7/6.
+//  Copyright (c) 2015年 徐继垚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
